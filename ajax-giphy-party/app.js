455ce2b338ee968459e095f1de9508a8e2e24a43
connertran <conner.tran@bsa.edu.vn>
2024-01-22 21:54:33 +0100

    styling the page and adding errors handling try catch

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -6,18 +6,33 @@ const deleteBtn = document.querySelector('.delete-btn');
 
 // using axios to connect with giphy api
 // getting the url of the image
-async function getGIF(searchGif){ 
-  const res = await axios.get('https://api.giphy.com/v1/gifs/search', {
-    params:{q:searchGif, api_key: 'F1dZfKez84ShwyjlOFdL9vgxn8STXAEy'}});
-  let randomIdx = Math.floor(Math.random() * Object.keys(res.data.data).length);
-  console.log(res)
-  console.log(res.data.data[randomIdx].url);
-  appendNewMeme(res.data.data[randomIdx].images.original.url);
+async function getGIF(searchGif){
+  try {
+    // the user forgot to write in the search bar
+    if (!searchGif.trim()) {
+      alert('Please enter a search item')
+      throw new Error('Please enter a search item');
+    }
+    const res = await axios.get('https://api.giphy.com/v1/gifs/search', {
+      params:{q:searchGif, api_key: 'F1dZfKez84ShwyjlOFdL9vgxn8STXAEy'}});
+    // the user misspelled the word in the search bar
+      if (res.data.data.length === 0) {
+      alert('No GIFs found for the given search item');
+      throw new Error('No GIFs found for the given search item');
+    }
+    let randomIdx = Math.floor(Math.random() * Object.keys(res.data.data).length);
+    console.log(res)
+    console.log(res.data.data[randomIdx].url);
+    appendNewMeme(res.data.data[randomIdx].images.original.url);
+}catch (error) {
+  console.error('Error:', error.message);
+}
 }
 
 function appendNewMeme(url){
   const newIMG = document.createElement('img');
   newIMG.src = url;
+  newIMG.classList.add('gif-size');
   memesArea.append(newIMG);
 }
 
